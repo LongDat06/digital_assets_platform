0ab8c98d4efd3d88651f129e17e72cbf305a20ba
LongDat06 <thanhdat24122000@gmail.com>
2025-05-05 09:46:59 +0700

    fix error

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class UsersController < BaseController
-      before_action :set_user, only: [:show, :update, :destroy]
+      before_action :set_user, only: [:show, :update]
       skip_before_action :authenticate_request, only: [:index]
 
       def index
